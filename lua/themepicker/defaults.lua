73d1f4276e65641e8c14953e59d48afb712a771e
paul <mail@paul-kleineberg.de>
2024-05-27 17:12:03 +0200

    definig default configs

--- lua/themepicker/defaults.lua
+++ lua/themepicker/defaults.lua
@@ -1,3 +1,15 @@
 return {
     test = true,
+
+    keys = {
+        {
+            mode = "n",
+            keys = "o",
+            command = "<cmd>lua require'lua.themepicker.loader'.applyColorScheme()<CR>",
+            opts = {
+                silent = true,
+                noremap = true,
+            }
+        }
+    },
 }
