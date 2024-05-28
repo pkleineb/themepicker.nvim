aae57f691c6cf8722ee1aae8d9e2b55012bfb6dc
paul <mail@paul-kleineberg.de>
2024-05-28 23:11:28 +0200

    feat: added unloading of atleast lua based colorschemes

--- lua/themepicker/utils.lua
+++ lua/themepicker/utils.lua
@@ -14,8 +14,7 @@ function M.getWinByBuffer(buffer)
     local windows = vim.api.nvim_list_wins()
     for _, win in ipairs(windows) do
         local winBuf = vim.api.nvim_win_get_buf(win)
-        if winBuf == buffer then
-            return win
+        if winBuf == buffer then return win
         end
     end
 
@@ -24,6 +23,30 @@ end
 
 function M.mergeConfig(baseConfig, newConfig)
     return vim.tbl_deep_extend("force", baseConfig, newConfig or {})
+end
+
+function M.diffTableKeys(startTable, endTable)
+    local diffTable = {}
+
+    for key, _ in pairs(endTable) do
+        if startTable[key] == nil then
+            table.insert(diffTable, key)
+        end
+    end
+
+    return diffTable
+end
+
+function M.parseStringTable(str)
+    -- striping all the values gone
+    local formattedStr = "return "
+    str = str:gsub("<1>", "")
+    for line in str:gmatch("[^\r\n]+") do
+        line = line:gsub("=.+$", "= '',")
+        formattedStr = formattedStr .. line .. "\n"
+    end
+
+    return loadstring(formattedStr)()
 end
 
 return M
